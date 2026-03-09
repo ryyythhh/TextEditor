50226a2e63f75f12f11e852d479eae03f0dbbe37
ryyythhh <pawarrhythm865@gmail.com>
2026-03-09 16:34:39 +0530

    Pushing text editor file.

---
+++ TextEditor.java
@@ -0,0 +1,30 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class TextEditor extends JFrame implements ActionListener {
+
+    JTextArea textArea;
+
+    public TextEditor() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Text Editor");
+        this.setSize(600, 700);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null); // Centers the window
+
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(450, 450));
+
+        this.add(textArea);
+        this.setVisible(true);
+    }
+
+    // This method is REQUIRED to fix the red error on line 6
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // You can add button logic here later
+    }
+}
